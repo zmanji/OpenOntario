2b7eae195628f2bf684807175ab21577bd14275e
Zameer Manji <zmanji@gmail.com>
2010-12-02 22:27:31 -0500

    Initial Commit

---
+++ README.rst
@@ -0,0 +1,4 @@
+======
+README
+======
+The Open Ontario Project aims to make available data about the Legislative Assembly of Ontario including bills, votes, debates and member information. This data is collected directly from the offical websites via a series of scrapers and made available in an easy to machine parse format. 
---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name = 'OpenOntario',
+    version='0.1dev',
+    packages=['openontario',],
+    license='GNU General Public License V3',
+    description="The Open Ontario Project",
+    author="Zameer Manji",
+    author_email="zmanji@gmail.com",
+    long_description=open('README.rst').read(),
+)
